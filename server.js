d0e31436a2a909d7e30100474f104b5784363747
afif1400 <afifahmed456123@gmail.com>
2021-11-18 06:50:27 -0800

    🐛 FIX: dynamic port

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+const PORT = process.env.PORT | 3000;
+
 app.get("/", (req, res) => {
   const code = `Welcome to WasteBin!
 
@@ -61,6 +63,6 @@ app.get("/:id", async (req, res) => {
   }
 });
 
-app.listen("3000", () => {
-  console.log("server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
